e41bc5295ccf889c8c159c6a931ed25b6b18dbd6
Carlos A. Peralta Alvarez <cperalta22@users.noreply.github.com>
2023-02-09 16:17:04 -0600

    primera_version

---
+++ pollo.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+#Carlos Peralta
+#2023
+#GPL3
+
+# text color params
+error=`tput setaf 1`
+info=`tput setaf 2`
+warn=`tput setaf 3`
+colorbg=`tput setab 1`
+
+# config file reading
+if [ -f ./pollo.conf ]; then
+    echo -e "${info}[info] Config file found: $(pwd)/pollo.conf ${reset}\n"
+    source ./pollo.conf
+elif [ -f /etc/pollo.conf ]; then
+    echo -e "${info}[info] Config file found: /etc ${reset}\n"
+    source /etc/pollo.conf
+else
+    echo "${error}[error] ./pollo.conf not found...bye${reset}"
+    exit
+fi
+
+# basic metrics
+(timeout 3s free -g || echo "timeout" > free.tmp ) | tr -s ' ' > free.tmp
+totmem=$((grep -i 'mem' free.tmp || echo "fail" ) | cut -f 2 -d ' ')
+usemem=$((grep -i 'mem' free.tmp || echo "fail" ) | cut -f 3 -d ' ')
+availablemem=$((grep -i 'mem' free.tmp || echo "fail")  | cut -f 7 -d ' ')
+totswa=$((grep -i 'swa' free.tmp || echo "fail") | cut -f 2 -d ' ')
+useswa=$((grep -i 'swa' free.tmp || echo "fail" ) | cut -f 3 -d ' ') # uso de swap
+freswa=$((grep -i 'swa' free.tmp || echo "fail" )| cut -f 4 -d ' ')
+rm free.tmp
+
+# new metrics
+if [ $availablemem != "fail" ] && [ $totmem != "fail" ]; then
+    availableMemPerc=$(($availablemem *100 / $totmem)) # esta es la buena para saber cuanta memoria queda libre
+else
+    availableMemPerc="fail"
+fi
+
+if [ $freswa != "fail" ] && [ $totswa != "fail" ]; then
+    freeSwapPerc=$(($freswa *100 / $totswa)) # esta es para el porcentaje del swap libre
+else
+    freeSwapPerc="fail"
+fi
+
+
+echo "POLLO INFORMA"
+echo "Cliente $(hostname)"
+echo "----------------------------------------------"
+echo "RAM"
+echo "Disponible $availableMemPerc % de $totmem GB"
+echo "SWAP libre $freeSwapPerc % de $totswa GB"
+echo $maxswap
